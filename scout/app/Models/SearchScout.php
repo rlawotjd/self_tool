7b36932d0d29d330d916368bc4741288be4852bc
rlawotjd <rlawotjd026@gmail.com>
2024-02-25 19:27:51 +0900

    scout

--- scout/app/Models/SearchScout.php
+++ scout/app/Models/SearchScout.php
@@ -6,9 +6,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Scout\Searchable;
 
-class Test extends Model
+class SearchScout extends Model
 {
-    use HasFactory;
     use Searchable;
-
+    use HasFactory;
 }
